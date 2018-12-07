f41c7b0145f3df7f10687d8323597f1bd880bca7
Bram De Coninck <bramdeconinck3@gmail.com>
2018-12-07 16:42:07 +0100

    Modified the getServices function to return an array of Service objects

--- Technology Salesman Toolkit/Technology Salesman Toolkit/Network/FirestoreAPI.swift
+++ Technology Salesman Toolkit/Technology Salesman Toolkit/Network/FirestoreAPI.swift
@@ -4,11 +4,15 @@ import Firebase
 struct FirestoreAPI {
     
     // https://firebase.google.com/docs/firestore/quickstart
-    func getServices() {
+    func getServices() -> [Service]? {
+        
+        // Firestore settings to use Timestamps instead of Date objects
         let db = Firestore.firestore()
         let settings = db.settings
         settings.areTimestampsInSnapshotsEnabled = true
         db.settings = settings
+        
+        var services: [Service] = []
         
         db.collection("Services").getDocuments() { (querySnapshot, err) in
             if let err = err {
@@ -17,12 +21,15 @@ struct FirestoreAPI {
                 for document in querySnapshot!.documents {
                     if let service = Service(dictionary: document.data(), id: document.documentID) {
                         print(service.name)
+                        services.append(service)
                     } else {
                         fatalError("Unable to initialize type \(Service.self) with dictionary \(document.data())")
                     }
                 }
             }
         }
+        
+        return services
     }
 
 }
