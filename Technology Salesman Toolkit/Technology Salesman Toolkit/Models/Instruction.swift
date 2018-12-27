1622f665a33993be0e51832b9669c3010db0f36e
Bram De Coninck <bramdeconinck3@gmail.com>
2018-12-27 11:30:35 +0100

    Made a small change to the Instruction model

--- Technology Salesman Toolkit/Technology Salesman Toolkit/Models/Instruction.swift
+++ Technology Salesman Toolkit/Technology Salesman Toolkit/Models/Instruction.swift
@@ -12,7 +12,7 @@ struct Instruction {
     let id: String
     let title: String
     let description: String
-    let content: String
+    let content: [String]
     let image: String
     let serviceId: String
     let index: Int
@@ -22,7 +22,7 @@ extension Instruction {
     init?(dictionary: [String : Any], id: String) {
         guard let title = dictionary["title"] as? String,
             let description = dictionary["description"] as? String,
-            let content = dictionary["content"] as? String,
+            let content = dictionary["content"] as? [String],
             let image = dictionary["image"] as? String,
             let serviceId = dictionary["serviceId"] as? String,
             let index = dictionary["index"] as? Int
