a1b4ae5a6b678a0c159c7af7ea7bc656daf169f7
Bram De Coninck <bramdeconinck3@gmail.com>
2019-01-12 18:47:52 +0100

    Created a basic layout for the profile screen

---
+++ Technology Salesman Toolkit/Technology Salesman Toolkit/Controllers/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Technology Salesman Toolkit
+//
+//  Created by Bram De Coninck on 12/01/2019.
+//  Copyright © 2019 Bram De Coninck. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
