3d58233afbe9fe7173f407ba1d1089f4cc2e4ab4
Bram De Coninck <bramdeconinck3@gmail.com>
2019-01-19 11:37:14 +0100

    Finished working on the layout of the profile screen

---
+++ Technology Salesman Toolkit/Technology Salesman Toolkit/Bases/BaseUIStackView.swift
@@ -0,0 +1,34 @@
+//
+//  BaseUIStackView.swift
+//  Technology Salesman Toolkit
+//
+//  Created by Bram De Coninck on 19/01/2019.
+//  Copyright © 2019 Bram De Coninck. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// https://stackoverflow.com/questions/32551890/how-to-add-leading-padding-to-view-added-inside-an-uistackview
+@IBDesignable
+class BaseUIStackView: UIStackView {
+    @IBInspectable private var color: UIColor?
+    override var backgroundColor: UIColor? {
+        get { return color }
+        set {
+            color = newValue
+            self.setNeedsLayout() // EDIT 2017-02-03 thank you @BruceLiu
+        }
+    }
+    
+    private lazy var backgroundLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        self.layer.insertSublayer(layer, at: 0)
+        return layer
+    }()
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        backgroundLayer.path = UIBezierPath(rect: self.bounds).cgPath
+        backgroundLayer.fillColor = self.backgroundColor?.cgColor
+    }
+}
